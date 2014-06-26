850ea56336138b7fbc91de6ab3cd61b7a0c4814d
Tim de Koning <git@king-management.nl>
2014-06-26 14:49:54 +0200

    added async test

---
+++ test/asyncConnectors.js
@@ -0,0 +1,34 @@
+//2
+var fs = require('fs');
+var when = require('when');
+var _ = require('lodash');
+describe('Async connectors', function (done) {
+	var apiKeysWithGroupIds, cbc;
+
+	this.timeout(10000);
+
+	try {
+		privateApiKeysWithGroupIds = JSON.parse(fs.readFileSync('./privateApiKeysWithGroupIds.json'));
+
+	} catch (e) {
+		console.log('** privateApiKeysWithGroupIds.json not found, skipping test **');
+		return;
+	}
+
+	cbc = require('../index.js');
+	it('should work with multiple connectors in multiple administratations', function (done) {
+		var promises = [], cbcs = {};
+
+		_.each(privateApiKeysWithGroupIds, function (groupId, apiKey) {
+			cbcs[apiKey] = cbc.clone(apiKey);
+			promises.push(cbcs[apiKey].getById('Group', groupId));
+		});
+		_.each(privateApiKeysWithGroupIds, function (groupId, apiKey) {
+			promises.push(cbcs[apiKey].getById('Group', groupId));
+		});
+
+		when.all(promises).then(function () {
+			done();
+		}, done);
+	});
+});

da49d32ffdfecb1f4422f293cb80faf10a669351
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2019-10-08 19:00:43 +0200

    fixed test

--- test/tests/misc.js
+++ test/tests/misc.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('misc', function () {
   it('can not set empty service url', () => {
     const cbc = require('../../lib/index.js').default;
-    assert.throws( cbc.setServiceUrl, Error, 'Cannot set empty service-url\'');
+    assert.throws(() => cbc.setServiceUrl(''), Error, 'Cannot set empty service-url');
   });
   it('can change service url', () => {
     const cbc = require('../../lib/index.js').default;
